c6cbe533597cc2a2b5d65e18bf3c58d198be9574
VERBRUGGEN Frédéric <tekodragoon@gmail.com>
2022-11-22 20:41:51 +0100

    add client side validation on user create

--- public/js/users.js
+++ public/js/users.js
@@ -26,6 +26,20 @@ const roleSelector = document.querySelector("#role");
 const openCreateDialog = document.querySelector("#new-user");
 const createUserForm = document.querySelector("#form-create-user");
 
+const lastNameErrFld = document.querySelector("#lastName-err-fld");
+const firstNameErrFld = document.querySelector("#firstName-err-fld");
+const birthdateErrFld = document.querySelector("#birthdate-err-fld");
+const roleSelectorErrFld = document.querySelector("#role-err-fld");
+const loginErrFld = document.querySelector("#login-err-fld");
+const passwordErrFld = document.querySelector("#password-err-fld");
+const formErrFld = document.querySelector("#form-err-fld");
+
+const lastName = document.querySelector("#lastName");
+const firstName = document.querySelector("#firstName");
+const birthdate = document.querySelector("#birthdate");
+const login = document.querySelector("#login");
+const password = document.querySelector("#password");
+
 for (let i = 0; i < deleteButtons.length; i++) {
   let prev = deleteButtons[i].previousElementSibling;
   let child = prev.firstElementChild;
@@ -92,7 +106,86 @@ roleSelector.addEventListener("change", () => {
       createUserForm.action = "/customerCreate";
       break;
   }
+  if (roleSelector.validity.valid) {
+    roleSelectorErrFld.textContent = "";
+    roleSelectorErrFld.className = "error-field";
+    checkErrors();
+  } else {
+    showError(roleSelector, roleSelectorErrFld);
+  }
 })
+
+createUserForm.addEventListener("submit", (ev) => {
+  if (!isFormValid()) {
+    ev.preventDefault();
+    showFormError();
+  }
+})
+
+lastName.addEventListener("input", (ev) => {
+  if (lastName.validity.valid) {
+    lastNameErrFld.textContent = "";
+    lastNameErrFld.className = "error-field";
+    checkErrors();
+  } else {
+    showError(lastName, lastNameErrFld);
+  }
+})
+
+firstName.addEventListener("input", (ev) => {
+  if (firstName.validity.valid) {
+    firstNameErrFld.textContent = "";
+    firstNameErrFld.className = "error-field";
+    checkErrors();
+  } else {
+    showError(firstName, firstNameErrFld);
+  }
+})
+
+birthdate.addEventListener("focus", birthdateHandler, false);
+birthdate.addEventListener("input", birthdateHandler, false);
+
+function birthdateHandler(ev) {
+  if (birthdate.validity.valid) {
+    birthdateErrFld.textContent = "";
+    birthdateErrFld.className = "error-field";
+    checkErrors();
+  } else {
+    showError(birthdate, birthdateErrFld);
+  }
+}
+
+roleSelector.addEventListener("focus", (ev) => {
+  if (roleSelector.validity.valid) {
+    roleSelectorErrFld.textContent = "";
+    roleSelectorErrFld.className = "error-field";
+    checkErrors();
+  } else {
+    showError(roleSelector, roleSelectorErrFld);
+  }
+})
+
+login.addEventListener("input", (ev) => {
+  if (login.validity.valid) {
+    loginErrFld.textContent = "";
+    loginErrFld.className = "error-field";
+    checkErrors();
+  } else {
+    showError(login, loginErrFld);
+  }
+})
+
+password.addEventListener("input", (ev) => {
+  if (password.validity.valid) {
+    passwordErrFld.textContent = "";
+    passwordErrFld.className = "error-field";
+    checkErrors();
+  } else {
+    showError(password, passwordErrFld);
+  }
+})
+
+// functions
 
 function openValidDeleteDialog(id) {
   deleteToken.value = id;
@@ -126,5 +219,44 @@ function openDialog(dialog) {
   } else {
     // console.log("navigateur incompatible avec dialog");
     dialog.show();
+  }
+}
+
+function isFormValid() {
+  if (!lastName.validity.valid) return false;
+  if (!firstName.validity.valid) return false;
+  if (!birthdate.validity.valid) return false;
+  if (discipline) {
+    if (!discipline.validity.valid) return false;
+  }
+  if (bio) {
+    if (!bio.validity.valid) return false;
+  }
+  return true;
+}
+
+function showError(field, errorField) {
+  if (field.validity.valueMissing) {
+    errorField.textContent = `Please enter your ${field.name.toLowerCase()}`;
+  }
+  if (field.validity.tooShort) {
+    errorField.textContent = `You need ${field.minLength} characters minimun`;
+  }
+  errorField.className = "error-field active";
+}
+
+function showFormError() {
+  formErrFld.textContent = "There are still errors on this form";
+  formErrFld.className = "error-field active";
+}
+
+function hideFormError() {
+  formErrFld.textContent = "";
+  formErrFld.className = "error-field";
+}
+
+function checkErrors() {
+  if (isFormValid()) {
+    hideFormError();
   }
 }
